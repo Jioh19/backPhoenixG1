02ac80e5fab4c6142e00d2d0cb0e60e24fbf9b56
Jioh19 <jioh1982@gmail.com>
2023-10-23 23:34:53 -0300

    add productoRouter get

--- routes/productoRouter.js
+++ routes/productoRouter.js
@@ -1,32 +1,55 @@
-const express = require("express");
+const express = require('express');
 const productoRouter = express.Router();
+const fs = require('fs');
 const {
-  crearProducto,
-  obtenerDetalleProducto,
-  obtenerProductos,
-  modificarProducto,
-  borrarProducto,
-} = require("../funciones");
+	crearProducto,
+	obtenerDetalleProducto,
+	obtenerProductos,
+	modificarProducto,
+	borrarProducto,
+} = require('../funciones');
 
-productoRouter.post("/", async (req, res) => {
-  // Implementa la lógica para crear un producto
+const URL = 'https://fakestoreapi.com/products';
+
+productoRouter.post('/', async (req, res) => {
+	// Implementa la lógica para crear un producto
 });
 
-productoRouter.get("/:id", async (req, res) => {
-  // Implementa la lógica para obtener el detalle de un producto
+productoRouter.get('/:id', async (req, res) => {
+	// Implementa la lógica para obtener el detalle de un producto
 });
 
-productoRouter.get("/", async (req, res) => {
-  console.log("PRODUCTO");
-  // Implementa la lógica para obtener todos los productos
+productoRouter.get('/', async (req, res) => {
+	console.log('PRODUCTO');
+	try {
+		const productsJson = fs.readFileSync('./DB/products.json');
+		if (productsJson == '') {
+			throw new Error('Empty');
+		}
+	} catch (error) {
+		if (error.code === 'ENOENT' || error.message === 'Empty') {
+			fetch(URL)
+				.then((res) => res.json())
+				.then((json) => {
+					listaProducts = json;
+					fs.writeFileSync(
+						'./DB/products.json',
+						JSON.stringify(listaProducts, null, 2)
+					);
+					return listaProducts;
+				})
+				.catch((error) => alert(error.message));
+		}
+	}
+	// Implementa la lógica para obtener todos los productos
 });
 
-productoRouter.put("/:id", async (req, res) => {
-  // Implementa la lógica para modificar un producto
+productoRouter.put('/:id', async (req, res) => {
+	// Implementa la lógica para modificar un producto
 });
 
-productoRouter.delete("/:id", async (req, res) => {
-  // Implementa la lógica para borrar un producto
+productoRouter.delete('/:id', async (req, res) => {
+	// Implementa la lógica para borrar un producto
 });
 
 module.exports = { productoRouter };
