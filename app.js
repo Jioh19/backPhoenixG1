a87ee14db4d8496628bcf78e92e0de0642f1502c
Jioh19 <jioh1982@gmail.com>
2023-10-25 19:08:11 -0300

    add cors

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { authRouter } = require("./routes/authRouter");
-const { usuarioRouter } = require("./routes/usuarioRouter");
-const { productoRouter } = require("./routes/productoRouter");
-const { carritoRouter } = require("./routes/carritoRouter");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { authRouter } = require('./routes/authRouter');
+const { usuarioRouter } = require('./routes/usuarioRouter');
+const { productoRouter } = require('./routes/productoRouter');
+const { carritoRouter } = require('./routes/carritoRouter');
+const cors = require('cors');
 const app = express();
 
 const PORT = 8080;
 
+app.use(cors());
+
 //AYUDA A QUE EL SERVIDOR ACEPTE PETICIONES POST
 app.use(express.json());
 app.use(bodyParser.json());
-
-app.use("/auth", authRouter);
-app.use("/producto", productoRouter);
-app.use("/usuario", usuarioRouter);
-app.use("/carrito", carritoRouter);
+app.use('/auth', authRouter);
+app.use('/producto', productoRouter);
+app.use('/usuario', usuarioRouter);
+app.use('/carrito', carritoRouter);
 
 app.listen(PORT, () => {
-  //LLAMADO A LA API https://fakestoreapi.com/products
-  console.log("conectado en el puerto: " + PORT);
+	//LLAMADO A LA API https://fakestoreapi.com/products
+	console.log('conectado en el puerto: ' + PORT);
 });
--- routes/productoRouter.js
+++ routes/productoRouter.js
@@ -8,24 +8,17 @@ const {
 	modificarProducto,
 	borrarProducto,
 } = require('../funciones');
+let listaProducts = [];
 
 const URL = 'https://fakestoreapi.com/products';
 
-productoRouter.post('/', async (req, res) => {
-	// Implementa la lógica para crear un producto
-});
-
-productoRouter.get('/:id', async (req, res) => {
-	// Implementa la lógica para obtener el detalle de un producto
-});
-
-productoRouter.get('/', async (req, res) => {
-	console.log('PRODUCTO');
+const loader = async () => {
 	try {
 		const productsJson = fs.readFileSync('./DB/products.json');
 		if (productsJson == '') {
 			throw new Error('Empty');
 		}
+		listaProducts = await JSON.parse(productsJson);
 	} catch (error) {
 		if (error.code === 'ENOENT' || error.message === 'Empty') {
 			fetch(URL)
@@ -36,20 +29,120 @@ productoRouter.get('/', async (req, res) => {
 						'./DB/products.json',
 						JSON.stringify(listaProducts, null, 2)
 					);
-					return listaProducts;
 				})
-				.catch((error) => alert(error.message));
+				.catch((err) => res.send(err));
 		}
 	}
+};
+
+productoRouter.post('/', async (req, res) => {
+	// Implementa la lógica para crear un producto
+	if (listaProducts.length === 0) {
+		await loader();
+	}
+	const body = req.body;
+	console.log(body);
+	const product = {
+		...body,
+		id: Number(listaProducts[listaProducts.length - 1].id) + 1,
+	};
+	listaProducts.push(product);
+	fs.writeFileSync(
+		'./DB/products.json',
+		JSON.stringify(listaProducts, null, 2)
+	);
+	return res.status(201).json(product);
+});
+
+productoRouter.get('/:id', async (req, res) => {
+	// Implementa la lógica para obtener el detalle de un producto
+	if (listaProducts.length === 0) {
+		await loader();
+	}
+
+	const id = req.params.id;
+	const product = listaProducts.filter((prod) => prod.id == id);
+	if (product == '') {
+		return res.status(404).json();
+	}
+	return res.status(200).json(product);
+});
+
+productoRouter.get('/', async (req, res) => {
 	// Implementa la lógica para obtener todos los productos
+	try {
+		const productsJson = fs.readFileSync('./DB/products.json');
+		if (productsJson == '') {
+			throw new Error('Empty');
+		}
+		listaProducts = JSON.parse(productsJson);
+
+		return res.status(200).json(listaProducts);
+	} catch (error) {
+		if (error.code === 'ENOENT' || error.message === 'Empty') {
+			fetch(URL)
+				.then((res) => res.json())
+				.then((json) => {
+					listaProducts = json;
+					fs.writeFileSync(
+						'./DB/products.json',
+						JSON.stringify(listaProducts, null, 2)
+					);
+					return res.status(200).json(listaProducts);
+				})
+				.catch((err) => res.send(err));
+		}
+	}
 });
 
 productoRouter.put('/:id', async (req, res) => {
 	// Implementa la lógica para modificar un producto
+	if (listaProducts.length === 0) {
+		await loader();
+	}
+
+	const id = req.params.id;
+	const body = req.body;
+	const product = listaProducts.find((prod) => prod.id == id);
+	const index = listaProducts.findIndex((prod) => prod.id == id);
+	console.log(index);
+	body.id = product.id;
+	body.title ||= product.title;
+	body.price ||= product.price;
+	body.description ||= product.description;
+	body.image ||= product.image;
+	body.category ||= product.category;
+	body.rating ||= product.rating;
+	listaProducts[index] = body;
+	console.log(body);
+	if (product === undefined) {
+		return res.status(404).json();
+	}
+	fs.writeFileSync(
+		'./DB/products.json',
+		JSON.stringify(listaProducts, null, 2)
+	);
+	return res.status(200).json(body);
 });
 
 productoRouter.delete('/:id', async (req, res) => {
 	// Implementa la lógica para borrar un producto
+	if (listaProducts.length === 0) {
+		await loader();
+	}
+
+	const id = req.params.id;
+	const product = listaProducts.filter((prod) => prod.id == id);
+	listaProducts = listaProducts.filter((prod) => prod.id != id);
+
+	if (product == '') {
+		return res.status(404).json();
+	}
+	fs.writeFileSync(
+		'./DB/products.json',
+		JSON.stringify(listaProducts, null, 2)
+	);
+	return res.status(204).json(product);
 });
 
 module.exports = { productoRouter };
